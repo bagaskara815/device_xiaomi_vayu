6f0580d4d9d8a88fb6ca636663b41a4e858c1abc
Bill Yi <byi@google.com>
2021-05-27 21:39:42 -0700

    vayu: Bypass build time check for users-libs
    * set PRODUCT_BROKEN_VERIFY_USES_LIBRARIES to true to bypss the build
    time check on users-libs to fix the build error.
    
    Bug: 189477034
    Test: pass the build
    Change-Id: I4343d46d4fe8fc1e24a3d7c9f4e88908bdc651bd

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -14,6 +14,12 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 # Inherit common AOSP configurations
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# b/189477034: Bypass build time check on uses_libs until vendor fixes all their apps
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+RELAX_USES_LIBRARY_CHECK := true
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
