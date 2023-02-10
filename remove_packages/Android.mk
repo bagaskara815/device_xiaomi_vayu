a742020fa3865accece1242484e839efb56e643b
bagaskara815 <bagaskara815@gmail.com>
2023-02-10 10:40:18 +0100

    vayu: remove_packages: remove some apps from gapps
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,10 +5,12 @@ LOCAL_MODULE := RemovePackagesVayu
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
     Chrome-Stub \
     Drive \
     FilesPrebuilt  \
     Maps \
+    PersonalSafety \
     YouTube
 
 LOCAL_UNINSTALLABLE_MODULE := true
