e365fa1a60f450d67e7a1af0e9be1a6eb9403690
Albert I <kras@raphielgang.org>
2024-06-10 12:34:27 +0000

    vayu: Inherit several Android Go configurations
    Beneficial for everyone, even on non-Go devices.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: Ib1d06ca810f98824865642cbf73ff2197058bae7

--- device.mk
+++ device.mk
@@ -308,6 +308,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/usr/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
 
+# Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # IMS & Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
