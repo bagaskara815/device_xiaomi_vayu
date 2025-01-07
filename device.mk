8b8a8443fda7655726e782fc0a84c1fa389676e9
Kelvin Zhang <zhangkelvin@google.com>
2025-01-07 07:08:33 +0000

    vayu: Explicitly set as an non-AB target We will be changing AB to be the default, existing non-AB targets need to explicitly set AB_OTA_UPDATER to false
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -590,6 +590,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service.pixel
 
+# Update
+AB_OTA_UPDATER := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
