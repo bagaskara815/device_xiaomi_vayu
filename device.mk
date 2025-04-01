77d40d38b9e0dd346ea8783cf5d34bb0369b6556
Pranav Vashi <neobuddy89@gmail.com>
2025-04-01 06:00:10 +0000

    vayu: dolby: Add profiles overlay for moto dolby
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -457,6 +457,9 @@ PRODUCT_PACKAGES += \
    TetheringConfigOverlayVayu \
    WifiOverlayVayu
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
