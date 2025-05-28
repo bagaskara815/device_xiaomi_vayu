91cb39653b1df22a9b110ec3e4640df80fda1587
bagaskara815 <bagaskara815@gmail.com>
2025-05-28 03:44:43 +0000

    vayu: Build thermal from source
    * Follow new hardware pixel, After commits:
    - https://github.com/LineageOS/android_hardware_google_pixel/commit/e4c2fa1fd8132a42fb827caa3291e8c4d054e75a
    
    - https://github.com/LineageOS/android_hardware_google_pixel/commit/fb95aa1c21ab85aa6c51843a34eb619565343e19
    
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -566,14 +566,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_vayu/com.nxp.mifare.xml
 
 # Thermal HAL
-SOONG_CONFIG_NAMESPACES += thermal_hal_feature
-SOONG_CONFIG_thermal_hal_feature += \
-    pid \
-
-SOONG_CONFIG_thermal_hal_feature_pid ?= apply_1_0
-
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.pixel \
+    android.hardware.thermal-service.pixel \
     thermal_symlinks
 
 PRODUCT_COPY_FILES += \
