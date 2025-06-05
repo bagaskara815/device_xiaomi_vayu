12df640aca66ed307ff863d067d779cb15c84f63
bagaskara815 <bagaskara815@gmail.com>
2025-06-05 11:15:37 +0000

    Revert "vayu: rro_overlay: Aperture -> Flash"
    This reverts commit ec85ba061a33a866c3740c460b055a76bd0b6f6a.
    
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -447,9 +447,9 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
+   ApertureOverlay \
    CarrierConfigOverlayVayu \
    DialerOverlayVayu \
-   FlashOverlay \
    FrameworksResOverlayVayu \
    NotchBarKillerVayu \
    SettingsOverlayVayu \
