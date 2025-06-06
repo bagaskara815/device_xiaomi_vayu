25275ac049e537ea501d1da395c6c30a0ae8e922
Michael Bestas <mkbestas@lineageos.org>
2025-06-06 04:05:15 +0000

    vayu: Disable OMX service
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -7,6 +7,7 @@
 # Inherit common products
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
