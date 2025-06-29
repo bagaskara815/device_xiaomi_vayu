7efe2ef06ef59b48b2bdb57ffe98851d283ea1eb
Michael Bestas <mkbestas@gmail.com>
2025-06-29 03:46:30 +0000

    vayu: Move Lineage Health HAL to select()
    - Its deprecated
    
    vendor/lineage/config/BoardConfigSoong.mk:34: warning: TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH is deprecated, please migrate to soong_config_set,lineage_health,charging_control_charging_path
    vendor/lineage/config/BoardConfigSoong.mk:42: warning: TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED is deprecated, please migrate to soong_config_set,lineage_health,charging_control_charging_enabled
    vendor/lineage/config/BoardConfigSoong.mk:46: warning: TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED is deprecated, please migrate to soong_config_set,lineage_health,charging_control_charging_disabled
    vendor/lineage/config/BoardConfigSoong.mk:50: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass
    vendor/lineage/config/BoardConfigSoong.mk:74: warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib
    [W][2025-05-11T16:15:28-0400][1258695] logParams():267 Process will be UID/EUID=0 in the global user namespace, and will have user root-level access to files
    [W][2025-05-11T16:15:28-0400][1258695] logParams():277 Process will be GID/EGID=0 in the global user namespace, and will have group root-level access to files
    vendor/lineage/config/BoardConfigSoong.mk:34: warning: TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH is deprecated, please migrate to soong_config_set,lineage_health,charging_control_charging_path
    vendor/lineage/config/BoardConfigSoong.mk:42: warning: TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED is deprecated, please migrate to soong_config_set,lineage_health,charging_control_charging_enabled
    vendor/lineage/config/BoardConfigSoong.mk:46: warning: TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED is deprecated, please migrate to soong_config_set,lineage_health,charging_control_charging_disabled
    vendor/lineage/config/BoardConfigSoong.mk:50: warning: TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS is deprecated, please migrate to soong_config_set,lineage_health,charging_control_supports_bypass
    
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -260,6 +260,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti
 
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,true)
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhwbinder.vendor
