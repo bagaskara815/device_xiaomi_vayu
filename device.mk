5e533c78d7b294aeecfd29637bdfdeaa0672a617
LuK1337 <priv.luk@gmail.com>
2026-02-20 04:56:03 +0700

    vayu: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Change-Id: Ic910f0634ee0707c21dad77f5264fd89fb33f1c0
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -63,6 +63,8 @@ PRODUCT_PACKAGES += \
     libprocessgroup.vendor \
     libtinycompress
 
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
